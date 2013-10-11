1cea9fad6ef26d59cc713bae25c4639cea033b2b
Trevor Stanhope <trevor@T510>
2013-10-10 21:32:02 -0400

    GUI basic text

--- code/MR15.py
+++ code/MR15.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+# Workstation System for the MR15
+# Run-Level 0: STANDBY
+# Run-Level 1: IGNITION
+# Run-Level 2: RUNNING
 import ast
 import serial
+import Tkinter as tk
+import time
 
-MONITOR_DEV = '/dev/ttyAMC0'
-CONTROLLER_DEV = '/dev/ttyAMC1'
-BAUD = 9600
+MONITOR_DEV = '/dev/ttyACM0'
+CONTROLLER_DEV = '/dev/ttyACM1'
+BAUD = 115200
 
 class MR15:
 
@@ -13,32 +19,105 @@ class MR15:
     try:
       self.ard_monitor = serial.Serial(MONITOR_DEV, BAUD)
       self.ard_controller = serial.Serial(CONTROLLER_DEV, BAUD)
+      self.runlevel = 0
     except Exception as error:
       print('--> ' + str(error))
     
   def monitor(self):
     print('[Monitoring Tractor]')
     try:
-      json = self.ard_monitor.read()
+      json = self.ard_monitor.readline()
       sensors = ast.literal_eval(json)
+      error = None
+      print('--> ' + str(json))
     except Exception as error:
+      sensors = {}
       print('--> ' + str(error))
-    return sensors
+    return (sensors, error)
     
-  def control(self, sensors):
+  def decide(self, sensors):
+    print('[Deciding Action]')
+    if sensors:
+      print('--> Working')
+      actions = 1
+    else:
+      print('--> Nothing')
+      actions = 0
+    return actions
+    
+  def control(self, actions):
     print('[Controlling Tractor]')
     try:
-      self.ard_controller.write()
+      self.ard_controller.write(actions)
+      error = None
     except Exception as error:
       print('--> ' + str(error))
+    return error
+      
+class Display(object):
+  def __init__(self, master, **kwargs):
+    print('[Initializing Display]')
+    pad = 3
+    self.master = master
+    self._geom='640x480+0+0'
+    master.geometry("{0}x{1}+0+0".format(master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+    self.set_layout() # set the UI layout
+                  
+  def set_layout(self):
+    print('[Setting Layout]')
+    ### Display
+    #self.master.overrideredirect(True) # make fullscreen
+    self.master.focus_set()
+    self.master.state("normal")
+    ### Error Label
+    self.error_var = tk.StringVar()
+    self.error_var.set('None')
+    error_label = tk.Label(self.master, textvariable=self.error_var, font=("Helvetica", 24))
+    error_label.pack()
+    error_label.place(x=20, y=20)
+    ### Loop-speed Label
+    self.speed_var = tk.StringVar()
+    self.error_var.set('0')
+    speed_label = tk.Label(self.master, textvariable=self.speed_var, font=("Helvetica", 24))
+    speed_label.pack()
+    speed_label.place(x=20, y=100)
+    ### Sensors Label
+    self.sensors_var = tk.StringVar()
+    self.sensors_var.set('0')
+    sensors_label = tk.Label(self.master, textvariable=self.sensors_var, font=("Helvetica", 24))
+    sensors_label.pack()
+    sensors_label.place(x=20, y=180)
+    
+  def update_error(self, error):
+    self.error_var.set(str(error))
+    self.master.update_idletasks()
+      
+  def update_speed(self, speed):
+    self.speed_var.set(str(speed))
+    self.master.update_idletasks()
+      
+  def update_sensors(self, speed):
+    self.sensors_var.set(str(speed))
+    self.master.update_idletasks()
     
 if __name__ == '__main__':
-  try:
-    tractor = MR15()
-    while True:
-      sensors = tractor.monitor()
-      tractor.control(sensors)
-  except Exception as error:
-    print('--> ' + str(error))
-  
 
+  ## Initialize objects
+  app = Display(tk.Tk())
+  tractor = MR15()
+  
+  ## Operation loop
+  while True:
+    a = time.time()
+    
+    (sensors, error) = tractor.monitor()
+    app.update_sensors(sensors)
+    app.update_error(error)
+    
+    actions = tractor.decide(sensors)
+    
+    error = tractor.control(actions)
+    app.update_error(error)
+    
+    b = time.time()
+    app.update_speed((b-a))
