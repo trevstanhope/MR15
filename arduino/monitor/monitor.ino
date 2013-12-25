66e41c9ef37f9f4051f495bb19267edd351137d0
Trevor Stanhope <trevor@W510>
2013-12-25 07:10:24 -0500

    changes

--- arduino/monitor/monitor.ino
+++ arduino/monitor/monitor.ino
@@ -5,14 +5,16 @@
 */
 
 /* --- Libraries --- */
+#include <SoftwareSerial.h> // needed for RFID module.
+#include "stdio.h" // needed for dtostrf()
+
 /* --- Pins --- */
-#define KILL_SEAT_PIN 2
-#define KILL_HITCH_PIN 3
-#define KILL_BUTTON_PIN 4
-#define LOCK_GUARD_PIN 5
-#define LOCK_RIGHTBRAKE_PIN 6
-#define LOCK_LEFTBRAKE_PIN 7
-#define IGNITION_PIN 8
+#define SEAT_PIN 2
+#define HITCH_PIN 3
+#define BUTTON_PIN 4
+#define GUARD_PIN 5
+#define BRAKE_PIN 6
+#define IGNITION_PIN 7
 
 /* --- Constants --- */
 const int BAUD = 115200; 
@@ -24,37 +26,48 @@ volatile int HITCH = 0;
 volatile int IGNITION = 0;
 volatile int BUTTON = 0;
 volatile int GUARD = 0;
-volatile int LEFTBRAKE = 0;
-volatile int RIGHTBRAKE = 0;
+volatile int BRAKE = 0;
 
 /* --- Setup --- */
 void setup() {
-  Serial.begin(BAUD);
-  pinMode(KILL_SEAT_PIN, INPUT);
-  pinMode(KILL_HITCH_PIN, INPUT);
-  pinMode(KILL_BUTTON_PIN, INPUT);
-  pinMode(LOCK_GUARD_PIN, INPUT);
-  pinMode(LOCK_LEFTBRAKE_PIN, INPUT);
-  pinMode(LOCK_RIGHTBRAKE_PIN, INPUT);
+
+  // Initialize Pins
+  pinMode(SEAT_PIN, INPUT);
+  pinMode(HITCH_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(GUARD_PIN, INPUT);
+  pinMode(BRAKE_PIN, INPUT);
   pinMode(IGNITION_PIN, INPUT);
-  digitalWrite(KILL_SEAT_PIN, HIGH);
-  digitalWrite(KILL_HITCH_PIN, HIGH);
-  digitalWrite(KILL_BUTTON_PIN, HIGH);
-  digitalWrite(LOCK_GUARD_PIN, HIGH);
-  digitalWrite(LOCK_LEFTBRAKE_PIN, HIGH);
-  digitalWrite(LOCK_RIGHTBRAKE_PIN, HIGH);
+
+  // Normally One - Enable Resistor
+  digitalWrite(SEAT_PIN, HIGH);
+  digitalWrite(HITCH_PIN, HIGH);
+  digitalWrite(BUTTON_PIN, HIGH);
+  digitalWrite(GUARD_PIN, HIGH);
+  digitalWrite(BRAKE_PIN, HIGH);
   digitalWrite(IGNITION_PIN, HIGH);
+
+  // Initialize Serial
+  delay(1000);
+  Serial.begin(BAUD);
+
 }
 
 /* --- Loop --- */
 void loop() {
-  SEAT = digitalRead(KILL_SEAT_PIN);
-  HITCH = digitalRead(KILL_HITCH_PIN);
-  BUTTON = digitalRead(KILL_BUTTON_PIN);
-  GUARD = digitalRead(LOCK_GUARD_PIN);
-  LEFTBRAKE = digitalRead(LOCK_LEFTBRAKE_PIN);
-  RIGHTBRAKE = digitalRead(LOCK_RIGHTBRAKE_PIN);
-  sprintf(SENSORS, "{'seat':%d,'hitch':%d,'button':%d,'left_brake':%d,'right_brake':%d,'guard':%d,'ignition':%d}",SEAT,HITCH,BUTTON,LEFTBRAKE,RIGHTBRAKE,GUARD,IGNITION); // concatenate message string
+  
+  // Read Pins
+  SEAT = digitalRead(SEAT_PIN);
+  HITCH = digitalRead(HITCH_PIN);
+  BUTTON = digitalRead(BUTTON_PIN);
+  GUARD = digitalRead(GUARD_PIN);
+  BRAKE = digitalRead(BRAKE_PIN);
+  IGNITION = digitalRead(IGNITION_PIN);
+
+  // Convert to string
+  sprintf(SENSORS, "{'seat':%d,'hitch':%d,'button':%d,'brake':%d,'guard':%d,'ignition':%d}",SEAT,HITCH,BUTTON,BRAKE,GUARD,IGNITION); // concatenate message string
+
+  // Send over serial
   Serial.println(SENSORS);
   Serial.flush();
 }
