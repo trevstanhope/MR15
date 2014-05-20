b969653d1496b1a1be28285ac79a4c3e640a1ba0
Trevor Stanhope <tpstanhope@gmail.com>
2014-05-20 00:34:56 -0400

    changes

--- MR15.py
+++ MR15.py
@@ -49,11 +49,11 @@ class Tractor:
                 try:
                     response[key]
                 except Exception:
-                    response[key] = 0
+                    response[key] = None
             print('\t' + str(response))
             return response
         except Exception as error:
-            print('--> ' + str(error))
+            print('\t' + str(error))
             
     def check_controller(self):
         print('[Getting ECU State]')
@@ -64,11 +64,11 @@ class Tractor:
                 try:
                     response[key]
                 except Exception:
-                    response[key] = 0
+                    response[key] = None
             print('\t' + str(response))
             return response
         except Exception as error:
-            print('--> ' + str(error))
+            print('\t' + str(error))
 
 # Display system 
 class Display(object):
@@ -86,9 +86,21 @@ class Display(object):
         
     def set_layout(self):
         print('[Setting Layout]')
-        self.master.overrideredirect(True) # make fullscreen
+        #self.master.overrideredirect(True) # make fullscreen
         self.master.focus_set()
         self.master.state("normal")
+        
+        ## EMU Label
+        self.emu_var = tk.StringVar()
+        self.emu_var.set('EMU')
+        emu_label = tk.Label(
+            self.master,
+            textvariable=self.emu_var,
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
+        )
+        emu_label.pack()
+        emu_label.place(x=20, y=40)
         
         ## Fuel Label
         self.fuel_var = tk.StringVar()
@@ -96,21 +108,23 @@ class Display(object):
         fuel_label = tk.Label(
             self.master,
             textvariable=self.fuel_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         fuel_label.pack()
-        fuel_label.place(x=20, y=40)
+        fuel_label.place(x=20, y=80)
         
         ## Wheel Label
         self.wheel_var = tk.StringVar()
-        self.wheel_var.set('Wheel Rate (RPM): ?')
+        self.wheel_var.set('Engine Rate (RPM): ?')
         wheel_label = tk.Label(
             self.master,
             textvariable=self.wheel_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         wheel_label.pack()
-        wheel_label.place(x=20, y=80)
+        wheel_label.place(x=20, y=120)
         
         ## Temperature Label
         self.temp_var = tk.StringVar()
@@ -118,10 +132,11 @@ class Display(object):
         temp_label = tk.Label(
             self.master,
             textvariable=self.temp_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         temp_label.pack()
-        temp_label.place(x=20, y=120)
+        temp_label.place(x=20, y=160)
         
         ## Humidity Label
         self.humidity_var = tk.StringVar()
@@ -129,21 +144,23 @@ class Display(object):
         humidity_label = tk.Label(
             self.master,
             textvariable=self.humidity_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         humidity_label.pack()
-        humidity_label.place(x=20, y=160)
+        humidity_label.place(x=20, y=200)
         
-        ## Frequency Label
-        self.freq_var = tk.StringVar()
-        self.freq_var.set('Frequency (Hz): ?')
-        freq_label = tk.Label(
+        ## ECU Label
+        self.ecu_var = tk.StringVar()
+        self.ecu_var.set('ECU')
+        ecu_label = tk.Label(
             self.master,
-            textvariable=self.freq_var,
-            font=("Helvetica", 24)
+            textvariable=self.ecu_var,
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
-        freq_label.pack()
-        freq_label.place(x=20, y=200)
+        ecu_label.pack()
+        ecu_label.place(x=20, y=280)
         
         ## State Label
         self.state_var = tk.StringVar()
@@ -151,54 +168,11 @@ class Display(object):
         state_label = tk.Label(
             self.master,
             textvariable=self.state_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         state_label.pack()
-        state_label.place(x=20, y=240)
-        
-        ## Brakes Label
-        self.brakes_var = tk.StringVar()
-        self.brakes_var.set('Brakes: ?')
-        brakes_label = tk.Label(
-            self.master,
-            textvariable=self.brakes_var,
-            font=("Helvetica", 24)
-        )
-        brakes_label.pack()
-        brakes_label.place(x=20, y=280)
-        
-        ## Guard Label
-        self.guard_var = tk.StringVar()
-        self.guard_var.set('CVT Guard: ?')
-        guard_label = tk.Label(
-            self.master,
-            textvariable=self.guard_var,
-            font=("Helvetica", 24)
-        )
-        guard_label.pack()
-        guard_label.place(x=20, y=320)
-        
-        ## Seat Label
-        self.seat_var = tk.StringVar()
-        self.seat_var.set('Seat: ?')
-        seat_label = tk.Label(
-            self.master,
-            textvariable=self.seat_var,
-            font=("Helvetica", 24)
-        )
-        seat_label.pack()
-        seat_label.place(x=20, y=360)
-        
-        ## Hitch Label
-        self.hitch_var = tk.StringVar()
-        self.hitch_var.set('Hitch: ?')
-        hitch_label = tk.Label(
-            self.master,
-            textvariable=self.hitch_var,
-            font=("Helvetica", 24)
-        )
-        hitch_label.pack()
-        hitch_label.place(x=20, y=400)
+        state_label.place(x=20, y=320)
         
         ## Steering Sensitivity Label
         self.steering_var = tk.StringVar()
@@ -206,10 +180,11 @@ class Display(object):
         steering_label = tk.Label(
             self.master,
             textvariable=self.steering_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         steering_label.pack()
-        steering_label.place(x=20, y=440)
+        steering_label.place(x=20, y=360)
         
         ## Ballast Speed Label
         self.ballast_var = tk.StringVar()
@@ -217,27 +192,42 @@ class Display(object):
         ballast_label = tk.Label(
             self.master,
             textvariable=self.ballast_var,
-            font=("Helvetica", 24)
+            font=("Helvetica", 24),
+            bg="#FFFFFF"
         )
         ballast_label.pack()
-        ballast_label.place(x=20, y=480)
+        ballast_label.place(x=20, y=400)
         
-    def update(self, monitor, control, freq):
+    def update(self, monitor, control):
         print('[Updating Display]')
         if (monitor):
             self.fuel_var.set('Fuel Rate (L/H): ' + str(monitor['fuel']))
             self.wheel_var.set('Wheel Rate (RPM): ' + str(monitor['wheel']))
             self.temp_var.set('Temperature (C): ' + str(monitor['temp']))
             self.humidity_var.set('Humidity (RH): ' + str(monitor['humidity']))
+        else:
+            print('\t NO EMU DATA')
         if (control):
-            self.state_var.set('State: ' + str(control['state']))
-            self.brakes_var.set('Brakes: ' + str(control['brakes']))
-            self.seat_var.set('Seat: ' + str(control['seat']))
-            self.hitch_var.set('Hitch: ' + str(control['hitch']))
-            self.guard_var.set('CVT Guard: ' + str(control['guard']))
-            self.steering_var.set('Steering Sensitivity: ' + str(control['steering']))
-            self.ballast_var.set('Ballast Speed: ' + str(control['ballast']))
-        self.freq_var.set('Frequency (Hz): ' + str(freq))
+            try:
+                if control['state'] == 0:
+                    state = 'OFF'
+                elif control['state'] == 1:
+                    state = 'STANDBY'
+                elif control['state'] == 2:
+                    state = 'RUNNING'
+                self.state_var.set('State: ' + state)
+            except Exception:
+                print('\t NO STATE FOUND')
+            try:
+                self.steering_var.set('Steering Sensitivity: ' + str(control['steering']))
+            except Exception:
+                print('\t NO STEERING SENSITIVITY FOUND')
+            try:
+                self.ballast_var.set('Ballast Speed: ' + str(control['ballast']))
+            except Exception:
+                print('\t NO BALLAST SPEED FOUND')
+        else:
+            print('\t NO ECU DATA')
         self.master.update_idletasks()
 
 # Main Loop
@@ -248,11 +238,8 @@ if __name__ == '__main__':
     tractor = Tractor()
     while True:
         try:
-            a = time.time()
             monitor = tractor.check_monitor()
             control = tractor.check_controller()
-            b = time.time()
-            freq = int(1/(b-a))
-            display.update(monitor, control, freq)
+            display.update(monitor, control)
         except KeyboardInterrupt:
             break
