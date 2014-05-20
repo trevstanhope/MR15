10b872e41347bb47e2a2721c57147803082e83fe
Trevor Stanhope <tpstanhope@gmail.com>
2014-05-20 01:16:53 -0400

    added alamode setup

--- install.sh
+++ install.sh
@@ -28,13 +28,13 @@ sudo cp config/lightdm.conf /etc/lightdm/lightdm.conf
 
 # Alamode
 sudo apt-get install arduino arduino-mk -y
-sudo cp configs/avrdude /usr/bin/avrdude
-sudo cp configs/avrdude /usr/share/arduino/hardware/tools
-sudo cp configs/avrdude.conf  /usr/share/arduino/hardware/tools
-sudo cp configs/boards.txt  /usr/share/arduino/hardware/arduino
-sudo cp configs/cmdline.txt /boot
-sudo cp configs/inittab /etc
-sudo cp configs/80-alamode.rules /etc/udev/rules.d
+sudo cp config/avrdude /usr/bin/avrdude
+sudo cp config/avrdude /usr/share/arduino/hardware/tools
+sudo cp config/avrdude.conf  /usr/share/arduino/hardware/tools
+sudo cp config/boards.txt  /usr/share/arduino/hardware/arduino
+sudo cp config/cmdline.txt /boot
+sudo cp config/inittab /etc
+sudo cp config/80-alamode.rules /etc/udev/rules.d
 sudo chown root /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
 sudo chgrp root /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
 sudo chmod a+s /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
